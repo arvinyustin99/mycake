b78c42a79a85d2bee3a289807cc15cdb8ca33f69
arvinyustin99 <arvinyustin99@gmail.com>
2021-08-28 17:40:50 +0700

    add static library to /public

---
+++ mycake-web/public/js/script.js
@@ -0,0 +1,9 @@
+function openNav() {
+    document.getElementById("navbar-menu").style.display = "block";
+    document.getElementById("menu-overlay-black").style.display = "block";
+}
+
+function closeNav() {
+    document.getElementById("navbar-menu").style.display = "none";
+    document.getElementById("menu-overlay-black").style.display = "none";
+}
