399ca4a76d5cfd45afda7ee44eabf5807e99c434
arvinyustin99 <arvinyustin99@gmail.com>
2021-08-28 17:58:07 +0700

    add card renderer of Half Hero

---
+++ mycake-web/components/HalfHero/index.tsx
@@ -0,0 +1,45 @@
+import { ReactComponentElement } from "react";
+
+interface Props {
+    title: String;
+    description?: String;
+}
+
+const HalfHero = (param: Props) => {
+    const { title } = param;
+    const { description } = param;
+
+    const RenderIfDescriptionExist = (): JSX.Element => {
+        if (description) {
+            return (
+                <div className="container">
+                    <div className="row">
+                        <div className="col-xl-10 col-lg-8 col-md-8 col-12 centered">
+                            <h1>{title}</h1>
+                            <h1>Berita & Promo</h1>
+                            <p>{description}</p>
+                            <p>Tidak hanya sekadar oleh-oleh, berjuta rasa dan kreativitas dalam sebuah piring.</p>
+                        </div>
+                    </div>
+                </div>
+            );
+        }
+        else {
+            return (
+                <div className="half-hero-title">
+                    <h1>Menu kami</h1>
+                    <h1>{title && title === '' ? title : ""}</h1>
+                </div>
+            );
+        }
+    }
+    return (
+        <div className="half-hero">
+            <div className="half-hero-background">
+                <RenderIfDescriptionExist/>
+            </div>
+        </div>
+    );
+}
+
+export default HalfHero;
